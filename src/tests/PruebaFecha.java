7f2234c56322d554ac1cff76b2ae50ad7974b644
Lucas <Lucas@192.168.1.37>
2017-03-05 17:25:42 +0100

    Segundo Commit (Branch 2)

---
+++ src/tests/PruebaFecha.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fecha.Fecha;
+
+public class PruebaFecha {
+
+	@Test
+	public void test() {
+		Fecha fecha;
+		
+		fecha = new Fecha(1,1,1111);
+		assertEquals(true, fecha.valida());
+		// día no válido
+		fecha = new Fecha(32,12,1111);
+		assertEquals(false, fecha.valida());
+		// mes no válido
+		fecha = new Fecha(1,13,1111);
+		assertEquals(false, fecha.valida());
+		// año no válido
+		fecha = new Fecha(1,1,-1111);
+		assertEquals(false, fecha.valida());
+		// día no válido con respecto al mes
+		fecha = new Fecha(31,4,1111);
+		assertEquals(false, fecha.valida());
+		//día no válido febrero no bisiesto
+		fecha = new Fecha(29,2,2017);
+		assertEquals(false, fecha.valida());
+		// día válido febrero bisiesto
+		fecha = new Fecha(29,2,2016);
+		assertEquals(true, fecha.valida());
+	}
+
+}
